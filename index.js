62185649b2eec0f36050e32d7eae263a3b5702ff
DESKTOP-G7VBR0R\WEIMOB <zhigang.zhang@weimob.com>
2022-04-14 12:36:06 +0800

    1

---
+++ index.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+const JsZip = require('jszip');
+
+// 读取目录及文件
+function readDir(obj, nowPath, nowFolder) {
+    // 读取目录中的所有文件及文件夹
+    const files = fs.readdirSync(nowPath);
+    files.forEach((fileName) => {
+        // 遍历检测目录中的文件
+        const fillPath = `${nowPath}/${fileName}`;
+        const file = fs.statSync(fillPath);
+        if (file.isDirectory()) {
+            // 如果是目录的话，继续查询
+            const folder = `${nowFolder}/${fileName}`;
+            // 压缩对象中生成该目录
+            const dirList = obj.folder(folder);
+            // 重新检索目录文件
+            readDir(dirList, fillPath, folder);
+        } else {
+            // 如果是文件压缩目录添加文件
+            obj.file(fileName, fs.readFileSync(fillPath));
+        }
+    });
+}
+
+function startZip(outputPath) {
+    const zip = new JsZip();
+    const pathSep = path.sep
+    const outputPathArr = outputPath.split(pathSep)
+    const zipName = `${outputPathArr[outputPathArr.length - 1]}-${new Date().getTime()}.zip`
+
+    readDir(zip, outputPath, '');
+    zip.generateAsync({
+        type: 'nodebuffer',
+        compression: 'DEFLATE',
+        compressionOptions: {
+            level: 9,
+        },
+    }).then((content) => {
+        fs.writeFileSync(path.join(outputPath, zipName), content, 'utf-8');
+        console.log('压缩完成');
+    })
+}
+
+class WebpackBuildCompressPlugin {
+
+    constructor(options={}) {}
+
+    apply(compiler) {
+        const pluginName = WebpackBuildCompressPlugin.name
+        compiler.hooks.assetEmitted.tap(pluginName, (file, { outputPath }) => {
+            startZip(outputPath)
+        })
+    }
+}
+
+module.exports = WebpackBuildCompressPlugin
